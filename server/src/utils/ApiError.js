e2e2bb428cd7aabb398fea8c987ccf00a3ef746d
Yatharth Upamnyu <yatharthupmanyu@gmail.com>
2026-01-11 00:36:42 +0530

    Minor typo fixes in ApiError.js

--- server/src/utils/ApiError.js
+++ server/src/utils/ApiError.js
@@ -5,7 +5,7 @@ class ApiError extends Error {
     errors = [],
     stack = ""
   ) {
-    supper(message);
+    super(message);
     this.statusCode = statusCode;
     (this.data = null),
       (this.message = message),
@@ -15,7 +15,7 @@ class ApiError extends Error {
     if (stack) {
       this.stack = stack;
     } else {
-      Error.captureStackTree(this, this.constructor);
+      Error.captureStackTrace(this, this.constructor);
     }
   }
 }
